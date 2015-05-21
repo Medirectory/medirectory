99728b111658d11071f167acaf4690dc20a1750c
Pete K <krautscheid@mitre.org>
2015-05-21 12:03:15 -0400

    Fixed typos

--- db/migrate/20150519204059_create_taxonomy_license.rb
+++ db/migrate/20150519204059_create_taxonomy_license.rb
@@ -1,10 +1,10 @@
 class CreateTaxonomyLicense < ActiveRecord::Migration
   def change
     create_table :taxonomy_licenses do |t|
-      t.string :taxonmy_code
+      t.string :taxonomy_code
       t.string :license_number
       t.string :license_number_state_code
-      t.string :primgary_taxonomy_switch
+      t.string :primary_taxonomy_switch
       t.references :entity, polymorphic: true, index: true
     end
   end
