8a56dfeeaa28ca224fd6905fdbe23eb33cccfd9a
Pete K <krautscheid@mitre.org>
2015-06-12 12:59:56 -0400

    Updated API to support specific search fields (name, location, etc)

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -3,7 +3,7 @@ require 'textacular/searchable'
 class Provider < ActiveRecord::Base 
   # Use Textactular for searching, an alternate would be https://github.com/Casecommons/pg_search
   def self.searchable_language ; 'simple' ; end
-  extend Searchable(:searchable_name, :searchable_content)
+  extend Searchable(:searchable_name, :searchable_location, :searchable_taxonomy, :searchable_content)
   has_one :mailing_address, as: :entity
   has_one :practice_location_address, as: :entity
   has_many :other_provider_identifiers, as: :entity
