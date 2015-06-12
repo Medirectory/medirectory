610222102e76e3205130239e994ecc76dc1a07cc
Pete K <krautscheid@mitre.org>
2015-06-12 13:20:36 -0400

    First pass at a taxonomies API (for populating a search dropdown)

---
+++ app/controllers/api/v1/taxonomies_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module V1
+
+    class TaxonomiesController < ApplicationController
+
+      def index
+        # NOTE: We'll need to figure out what's most useful on the front end, this will do for now
+        taxonomies = TaxonomyCode.all.map { |t| "#{t.classification}#{" (#{t.specialization})" if t.specialization}" }
+        respond_to do |format|
+          format.xml { render xml: taxonomies }
+          format.json { render json: MultiJson.encode(taxonomies: taxonomies) }
+        end
+      end
+
+    end
+
+  end
+end
