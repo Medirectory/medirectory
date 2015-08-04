107b717501f820be8cee059bebcf2e82da107ffd
Robert Horn <rhorn@mitre.org>
2015-08-04 15:33:30 -0400

    added in parsing wip

--- app/controllers/hpd/soap_controller.rb
+++ app/controllers/hpd/soap_controller.rb
@@ -5,15 +5,19 @@ module Hpd
 
     DSML = "urn:oasis:names:tc:DSML:2:0:core"
 
+    lookup_text = { givenName: "first_name", sn: "last_name_legal_name" }
+
     def wsdl
       respond_to :wsdl
     end
 
     def endpoint
       # default SOAP action
-      soap_message = Nokogiri::XML(request.body.read)
-      search_value = soap_message.xpath("//dsml:searchRequest/dsml:filter/dsml:equalityMatch/dsml:value/text()", "dsml" => DSML)
-      @providers = Provider.where(first_name: search_value.to_s.upcase).limit(10)
+      soap_message = Nokogiri::XML(request.body.read) do |config|
+        config.noblanks
+      end
+      batch_request = soap_message.xpath("//dsml:batchRequest", "dsml" => DSML)
+      parse_batch(batch_request)
       respond_to :soap
     end
 
@@ -30,6 +34,35 @@ module Hpd
       errors = schema.validate(soap_body).map{|e| e.message}.join(", ")
       raise(StandardError, errors) unless errors.empty?
     end
+
+    # TODO: handle undefined methods (i.e addRequest)
+    def parse_batch (batch_request)
+      batch_request.children.each do |request|
+        send(request.name.underscore, request)
+      end
+    end
+
+    def search_request (request)
+      filter = request.xpath("//dsml:filter", "dsml" => DSML)
+      query = parse (filter.first_element_child)
+      @providers = Provider.where(query.query, query.params).limit(10)
+    end
+
+    def parse (element)
+      vals = case element.name
+             when "equalityMatch"
+               {
+                 query: lookup_text[element.attr(:name).text().intern] + ' = :' + element.attr(:name).text,
+                 params: {element.attr(:name).text().intern => element.attr(:value).text}
+               }
+             else
+               {
+                 query: "",
+                 params: {}
+               }
+             end
+      return { query: '('+vals.query+')', params: vals.params }
+    end
 
   end
 end
